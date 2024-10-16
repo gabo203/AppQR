e3df99ed04d47235f3316e92250fa4cf34f9c381
gabo203 <ga.ibanez@duocuc.cl>
2024-10-16 01:09:19 -0300

    vistas de usuarios y validaciones en el registro

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -8,22 +8,95 @@ import { Router } from '@angular/router';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  username: string = "";
-  email: string = "";
-  password: string = "";
+  email: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  username: string = '';
+  passwordFieldType: string = 'password';
+  passwordIcon: string = 'eye-off-outline';
+  confirmPasswordFieldType: string = 'password';
+  confirmPasswordIcon: string = 'eye-off-outline';
+  passwordMismatch: boolean = false;
+  emailInvalid: boolean = false;
+  usernameInvalid: boolean = false;
+  private errorTimeout: any;
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit() {}
 
-  async register() {
-    try {
-      await this.authService.register(this.email, this.password, this.username);
-      alert("Usuario registrado exitosamente! " + this.email);
-      this.router.navigate(['/login']);
-    } catch (error) {
-      console.error("Problemas al registrar el usuario: " + error);
-      alert("Error al registrar el usuario. Por favor, intente nuevamente.");
+  register() {
+    this.passwordMismatch = this.password !== this.confirmPassword;
+    this.emailInvalid = this.email !== '' && !this.isValidEmail(this.email);
+    this.usernameInvalid = this.username !== '' && !this.isValidUsername(this.username);
+
+    if (this.passwordMismatch || this.emailInvalid || this.usernameInvalid) {
+      this.triggerShakeAnimation();
+      this.resetErrorTimeout();
+    } else {
+      this.authService.register(this.email, this.password, this.username)
+        .then(resultado => {
+          if (resultado) {
+            alert('¡Registro exitoso!');
+            this.router.navigate(['/login']);
+          } else {
+            alert('Error en el registro. Por favor, intente nuevamente.');
+          }
+        })
+        .catch(error => {
+          console.error('Error en el registro: ', error);
+          alert('Error en el registro. Por favor, verifique sus datos e intente nuevamente.');
+        });
+    }
+  }
+
+  isValidEmail(email: string): boolean {
+    return email.endsWith('@duocuc.cl') || email.endsWith('@profesor.duoc.cl');
+  }
+
+  isValidUsername(username: string): boolean {
+    const usernameRegex = /^[a-zA-Z0-9]+$/;
+    return usernameRegex.test(username);
+  }
+
+  triggerShakeAnimation() {
+    const errorMessageElements = document.querySelectorAll('.error-message');
+    errorMessageElements.forEach(element => {
+      const htmlElement = element as HTMLElement;
+      htmlElement.classList.remove('shake');
+      void htmlElement.offsetWidth; // Trigger reflow
+      htmlElement.classList.add('shake');
+    });
+  }
+
+  resetErrorTimeout() {
+    if (this.errorTimeout) {
+      clearTimeout(this.errorTimeout);
+    }
+    this.errorTimeout = setTimeout(() => {
+      this.passwordMismatch = false;
+      this.emailInvalid = false;
+      this.usernameInvalid = false;
+    }, 5000);
+  }
+
+  togglePasswordVisibility() {
+    if (this.passwordFieldType === 'password') {
+      this.passwordFieldType = 'text';
+      this.passwordIcon = 'eye-outline';
+    } else {
+      this.passwordFieldType = 'password';
+      this.passwordIcon = 'eye-off-outline';
+    }
+  }
+
+  toggleConfirmPasswordVisibility() {
+    if (this.confirmPasswordFieldType === 'password') {
+      this.confirmPasswordFieldType = 'text';
+      this.confirmPasswordIcon = 'eye-outline';
+    } else {
+      this.confirmPasswordFieldType = 'password';
+      this.confirmPasswordIcon = 'eye-off-outline';
     }
   }
 }
--- src/app/components/reset-password/reset-password.component.ts
+++ src/app/components/reset-password/reset-password.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { firstValueFrom } from 'rxjs';
+import { QuerySnapshot } from '@angular/fire/compat/firestore';
 
 @Component({
   selector: 'app-reset-password',
@@ -8,25 +11,48 @@ import { Router } from '@angular/router';
   styleUrls: ['./reset-password.component.scss'],
 })
 export class ResetPasswordComponent {
-  email: string = "";
-  currentPassword: string = "";
-  newPassword: string = "";
-  confirmPassword: string = "";
+  email: string = '';
+  currentPassword: string = '';
+  newPassword: string = '';
+  confirmPassword: string = '';
 
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private authService: AuthService, private router: Router, private firestore: AngularFirestore) {}
 
   async resetPassword() {
+    if (!this.email.endsWith('@duocuc.cl') && !this.email.endsWith('@profesor.duoc.cl')) {
+      alert('Solo se permite el restablecimiento de contraseña para correos que terminan en @duocuc.cl o @profesor.duoc.cl.');
+      return;
+    }
+
     try {
+      const emailExists = await this.checkEmailExists(this.email);
+      if (!emailExists) {
+        alert('El correo electrónico no está registrado. Por favor, verifica tu correo e intenta nuevamente.');
+        return;
+      }
+
       await this.authService.sendPasswordResetEmail(this.email);
-      alert("Se ha enviado un correo de restablecimiento de contraseña. Por favor, revisa tu bandeja de entrada.");
+      alert('Se ha enviado un correo de restablecimiento de contraseña. Por favor, revisa tu bandeja de entrada.');
       this.router.navigate(['/login']);
     } catch (error) {
-      console.error("Error al enviar el correo de restablecimiento: ", error);
-      alert("Error al enviar el correo de restablecimiento. Por favor, verifica tu correo electrónico e intenta nuevamente.");
+      console.error('Error al enviar el correo de restablecimiento: ', error);
+      alert('Error al enviar el correo de restablecimiento. Por favor, verifica tu correo electrónico e intenta nuevamente.');
+    }
+  }
+
+  private async checkEmailExists(email: string): Promise<boolean> {
+    try {
+      const userSnapshot: QuerySnapshot<any> = await firstValueFrom(
+        this.firestore.collection('users', ref => ref.where('email', '==', email)).get()
+      );
+      return !userSnapshot.empty;
+    } catch (error) {
+      console.error('Error al verificar la existencia del correo electrónico: ', error);
+      return false;
     }
   }
 
   goToLogin() {
-  this.router.navigate(['/login']);
-}
+    this.router.navigate(['/login']);
+  }
 }
